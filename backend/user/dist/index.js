c9a5aa971c38cff72926d0802f207d3def32ea05
Cshraddha1503 <shraddha76830@gmail.com>
2026-06-24 23:40:43 +0530

    user service

--- backend/user/dist/index.js
+++ backend/user/dist/index.js
@@ -3,27 +3,19 @@ import dotenv from "dotenv";
 import connectDb from './config/db.js';
 import { createClient } from 'redis';
 import userRoutes from './routes/user.js';
-
-
 dotenv.config();
 connectDb();
-
 export const redisClient = createClient({
     url: process.env.REDIS_URL,
 });
-
 redisClient
-.connect()
-.then(() => console.log("connected to redis"))
-.catch((err) => console.log(err));
-
-
+    .connect()
+    .then(() => console.log("connected to redis"))
+    .catch((err) => console.log(err));
 const app = express();
-
 app.use("api/v1", userRoutes);
-
 const port = process.env.PORT;
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+//# sourceMappingURL=index.js.map
