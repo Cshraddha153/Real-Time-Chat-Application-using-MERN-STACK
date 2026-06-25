7dd5b1dd3ea6dc67cbb88f3d241123a73142061e
Cshraddha1503 <shraddha76830@gmail.com>
2026-06-26 00:50:18 +0530

    Adding Mail Microservice

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 import connectDb from './config/db.js';
 import { createClient } from 'redis';
 import userRoutes from './routes/user.js';
+import { connectRabbitMQ } from './config/rabbitmq.js';
 
 
 dotenv.config();
 connectDb();
+
+connectRabbitMQ();
 
 export const redisClient = createClient({
     url: process.env.REDIS_URL!,
@@ -19,6 +22,7 @@ redisClient
 
 
 const app = express();
+app.use(express.json());
 
 app.use("api/v1", userRoutes);
 
