9589c0176950f61cfa28ddca540791e3ded72a98
Cshraddha1503 <shraddha76830@gmail.com>
2026-06-30 12:13:43 +0530

    adding functionality

--- backend/chat/src/index.ts
+++ backend/chat/src/index.ts
@@ -2,18 +2,20 @@ import express from "express";
 import dotenv from 'dotenv';
 import connectDB from "./config/db.js";
 import chatRoutes from './routes/chat.js'
+import { app, server } from "./config/socket.js";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
 
-const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1", chatRoutes);
 
 const port = process.env.PORT;
 
-app.listen(port, ()=>{
+server.listen(port, ()=>{
     console.log(`Server is running on ${port}`);
 })
